79c973b354a72a382a0f7e2a516f06820b47bef5
khaleedbinaziz1 <mayj4722@gmail.com>
2025-02-01 05:48:25 +0600

    shit

--- src/app/pages/login/firebase/firebase.js
+++ src/app/pages/login/firebase/firebase.js
@@ -1,36 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { getFirestore, collection, getDocs } from 'firebase/firestore'; // Add collection and getDocs
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore, onSnapshot } from 'firebase/firestore'; // Import onSnapshot
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-pEY384lM5P4Q5_wVoEwGxaQwIQLRWC0",
-  authDomain: "bytezle-4f2b0.firebaseapp.com",
-  projectId: "bytezle-4f2b0",
-  storageBucket: "bytezle-4f2b0.appspot.com",
-  messagingSenderId: "627420341833",
-  appId: "1:627420341833:web:358a649802273ac7d88198",
-  measurementId: "G-4PSTESRPM3"
+  apiKey: "AIzaSyD5ACh9BgXc_E6upc4_3Z8Qa9VKkkRBII0",
+  authDomain: "better-ecom.firebaseapp.com",
+  projectId: "better-ecom",
+  storageBucket: "better-ecom",
+  messagingSenderId: "1217353368",
+  appId: "1:1217353368:web:fd3760e646899ee35ea2ed",
+  measurementId: "G-NXP5W1X8HS"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-// Test Firestore connection
-async function testFirestoreConnection() {
-  try {
-    const querySnapshot = await getDocs(collection(db, 'testCollection'));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-    });
-    console.log('Firestore is connected and working!');
-  } catch (error) {
-    console.error('Error connecting to Firestore:', error);
-  }
-}
-
-testFirestoreConnection(); // Call the function to test the connection
-
-export { app, auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
+export { app, auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onSnapshot };
